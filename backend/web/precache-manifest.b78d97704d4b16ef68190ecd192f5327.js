6182d991e90c750cbbc18227e956f9dd696c6123
Abhay D <a.deshpande012@gmail.com>
2020-08-05 19:11:43 -0600

    Added build of frontend to backend static files

---
+++ backend/web/precache-manifest.b78d97704d4b16ef68190ecd192f5327.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c15e73291f46ad27cdf374dea295372",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80aecf512b47ac9f7f04",
+    "url": "/static/css/main.eb343a5b.chunk.css"
+  },
+  {
+    "revision": "8cf86bd80b3748ed82a2",
+    "url": "/static/js/2.cec1c89a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cec1c89a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80aecf512b47ac9f7f04",
+    "url": "/static/js/main.d16f8d23.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "a6a829fc284122eed861e7aee4a6eff1",
+    "url": "/static/media/back.a6a829fc.svg"
+  }
+]);
